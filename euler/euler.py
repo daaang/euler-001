667819c9a74bd7ee1dbfe8047b165f9ca33a11f9
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-06 10:46:53 -0500

    Extract class

--- euler/euler.py
+++ euler/euler.py
@@ -1,3 +1,13 @@
+class MultipleSummer:
+
+    def __init__ (self, *args):
+        self.factors = list(args)
+        self.ceiling = self.factors.pop()
+
+    def __call__ (self):
+        return sum(unique_multiples_under_ceiling(
+                self.ceiling, self.factors))
+
 def range_without_duplicates (ceiling, factor, factors):
     for i in range(0, ceiling, factor):
         if all(i%j != 0 for j in factors):
@@ -11,7 +21,5 @@ def unique_multiples_under_ceiling (ceiling, factors):
         yield from range_without_duplicates(ceiling, factor, factors)
 
 def sum_of_multiples (*args):
-    factors = list(args)
-    ceiling = factors.pop()
-
-    return sum(unique_multiples_under_ceiling(ceiling, factors))
+    x = MultipleSummer(*args)
+    return x()
