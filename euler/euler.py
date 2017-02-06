a0778f9d463039221f345e2332cc50e2007be501
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-06 10:48:31 -0500

    Move unique_multiples_under_ceiling() into class

--- euler/euler.py
+++ euler/euler.py
@@ -5,20 +5,19 @@ class MultipleSummer:
         self.ceiling = self.factors.pop()
 
     def __call__ (self):
-        return sum(unique_multiples_under_ceiling(
-                self.ceiling, self.factors))
+        return sum(self.unique_multiples_under_ceiling())
+
+    def unique_multiples_under_ceiling (self):
+        factors = list(self.factors)
+
+        while factors:
+            factor = factors.pop(0)
+            yield from range_without_duplicates(self.ceiling, factor, factors)
 
 def range_without_duplicates (ceiling, factor, factors):
     for i in range(0, ceiling, factor):
         if all(i%j != 0 for j in factors):
             yield i
-
-def unique_multiples_under_ceiling (ceiling, factors):
-    factors = list(factors)
-
-    while factors:
-        factor = factors.pop(0)
-        yield from range_without_duplicates(ceiling, factor, factors)
 
 def sum_of_multiples (*args):
     x = MultipleSummer(*args)
