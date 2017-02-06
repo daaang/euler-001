05017109dcb745569f52f2c38f31c06093263e1d
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-06 09:09:21 -0500

    The sums below zero and one should always be zero

--- euler/test_euler.py
+++ euler/test_euler.py
@@ -11,13 +11,13 @@ class SumOfMultiplesTestBase (unittest.TestCase):
         args = tuple(self.factors + [ceiling])
         assert_that(sum_of_multiples(*args), is_(equal_to(expected)))
 
-class GivenNoFactors (SumOfMultiplesTestBase):
-
     def test_sum_below_zero_is_zero (self):
         self.assert_sum(0, 0)
 
     def test_sum_below_one_is_zero (self):
         self.assert_sum(1, 0)
+
+class GivenNoFactors (SumOfMultiplesTestBase):
 
     def test_sum_below_1000_is_zero (self):
         self.assert_sum(1000, 0)
@@ -25,12 +25,6 @@ class GivenNoFactors (SumOfMultiplesTestBase):
 class GivenFactorOfOne (SumOfMultiplesTestBase):
 
     factors = [1]
-
-    def test_sum_below_zero_is_zero (self):
-        self.assert_sum(0, 0)
-
-    def test_sum_below_one_is_zero (self):
-        self.assert_sum(1, 0)
 
     def test_sum_below_two_is_one (self):
         self.assert_sum(2, 1)
