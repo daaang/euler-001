6937df7ebc7f3a80a033aba976880cca0385e947
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-06 09:24:22 -0500

    No longer need to test for values in factors

--- euler/euler.py
+++ euler/euler.py
@@ -2,5 +2,4 @@ def sum_of_multiples (*args):
     factors = list(args)
     ceiling = factors.pop()
 
-    return sum(sum(range(0, ceiling, f)) for f in factors) \
-            if factors else 0
+    return sum(sum(range(0, ceiling, f)) for f in factors)
