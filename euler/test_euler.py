e85daac20676557db58d8a2712e7e5da146d0f32
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-06 09:02:03 -0500

    Refactor tests

--- euler/test_euler.py
+++ euler/test_euler.py
@@ -3,21 +3,31 @@ import unittest
 
 from .euler import sum_of_multiples
 
-class GivenNoFactors (unittest.TestCase):
+class SumOfMultiplesTestBase (unittest.TestCase):
+
+    factors = [ ]
+
+    def assert_sum (self, ceiling, expected):
+        args = tuple(self.factors + [ceiling])
+        assert_that(sum_of_multiples(*args), is_(equal_to(expected)))
+
+class GivenNoFactors (SumOfMultiplesTestBase):
 
     def test_sum_up_to_zero_is_zero (self):
-        assert_that(sum_of_multiples(0), is_(equal_to(0)))
+        self.assert_sum(0, 0)
 
     def test_sum_up_to_one_is_zero (self):
-        assert_that(sum_of_multiples(1), is_(equal_to(0)))
+        self.assert_sum(1, 0)
 
     def test_sum_up_to_1000_is_zero (self):
-        assert_that(sum_of_multiples(1000), is_(equal_to(0)))
+        self.assert_sum(1000, 0)
 
-class GivenFactorOfOne (unittest.TestCase):
+class GivenFactorOfOne (SumOfMultiplesTestBase):
+
+    factors = [1]
 
     def test_sum_up_to_zero_is_zero (self):
-        assert_that(sum_of_multiples(1, 0), is_(equal_to(0)))
+        self.assert_sum(0, 0)
 
     def test_sum_up_to_one_is_one (self):
-        assert_that(sum_of_multiples(1, 1), is_(equal_to(1)))
+        self.assert_sum(1, 1)
