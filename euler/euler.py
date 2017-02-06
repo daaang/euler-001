79c17f03c166adc48b55b694f7d90967023195c9
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-06 10:32:25 -0500

    Refactor to make uglier but also more changeable

--- euler/euler.py
+++ euler/euler.py
@@ -2,4 +2,11 @@ def sum_of_multiples (*args):
     factors = list(args)
     ceiling = factors.pop()
 
-    return sum(sum(range(0, ceiling, f)) for f in factors)
+    result = 0
+    while factors:
+        factor = factors.pop(0)
+        for i in range(0, ceiling, factor):
+            if all(i%j != 0 for j in factors):
+                result += i
+
+    return result
