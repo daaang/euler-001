c80f7997c7cb941d2ccdd63a0faedae19b2e0bda
Matthew Alexander LaChance <matt@istheguy.com>
2017-02-06 10:35:57 -0500

    Extract method

--- euler/euler.py
+++ euler/euler.py
@@ -1,3 +1,8 @@
+def range_without_duplicates (ceiling, factor, factors):
+    for i in range(0, ceiling, factor):
+        if all(i%j != 0 for j in factors):
+            yield i
+
 def sum_of_multiples (*args):
     factors = list(args)
     ceiling = factors.pop()
@@ -5,8 +10,8 @@ def sum_of_multiples (*args):
     result = 0
     while factors:
         factor = factors.pop(0)
-        for i in range(0, ceiling, factor):
-            if all(i%j != 0 for j in factors):
-                result += i
+        result += sum(range_without_duplicates(ceiling,
+                                               factor,
+                                               factors))
 
     return result
